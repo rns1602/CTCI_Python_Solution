28f4fa4e7d86787534e57ae209eec7401d3ede17
Rushit <rshah231@uic.edu>
2019-06-18 23:52:58 -0500

    Added Q1.3 to strings_arrays.py

--- Chpt1_Arrays_Strings/strings_arrays.py
+++ Chpt1_Arrays_Strings/strings_arrays.py
@@ -5,6 +5,7 @@ Created on Tue Jun 18 02:39:32 2019
 
 @author: rushit
 """
+
 "Chpt1 - Strings & Arrays"
 
 "Q1.1: Implement an algorihm to determine if a string has all unique characters"
@@ -46,3 +47,73 @@ def reverse_c_string(string):
         return string_rev
     else:
         print("Please enter a valid string")
+
+"Q1.2: Design an algorithm and write code to remove the duplicate characters in" 
+"a string without using any additional buffer NOTE: One or two additional" 
+"variables are fine An extra copy of the array is not."
+
+class CharCarrier():
+    
+    def __init__(self,ind,char):
+        self.index = ind
+        self.value = ord(char)
+        self.dupe = False
+        return
+        
+    def get_char(self):
+        return chr(self.value)
+
+def merge_sort(arr):
+    "https://www.geeksforgeeks.org/merge-sort/"
+    if len(arr) >1: 
+        mid = len(arr)//2 #Finding the mid of the array 
+        L = arr[:mid] # Dividing the array elements  
+        R = arr[mid:] # into 2 halves 
+  
+        merge_sort(L) # Sorting the first half 
+        merge_sort(R) # Sorting the second half 
+  
+        i = j = k = 0
+          
+        # Copy data to temp arrays L[] and R[] 
+        while i < len(L) and j < len(R): 
+            if L[i].value < R[j].value: 
+                arr[k] = L[i] 
+                i+=1
+            else: 
+                arr[k] = R[j] 
+                j+=1
+            k+=1
+
+        # Checking if any element was left 
+        while i < len(L): 
+            arr[k] = L[i] 
+            i+=1
+            k+=1
+          
+        while j < len(R): 
+            arr[k] = R[j] 
+            j+=1
+            k+=1
+
+def remove_duplicates_string(string):
+    
+    string_new = []
+    
+    for i in range(len(string)):
+        s = CharCarrier(i, string[i])
+        string_new.append(s)
+
+    merge_sort(string_new)
+    
+    string_final = []
+    string_final.append(chr(string_new[0].value))
+    for i in range(len(string_new)-1):
+        if string_new[i].value is not string_new[i+1].value:
+            string_final.append(chr(string_new[i+1].value))
+    
+    return string_final
+        
+x = remove_duplicates_string("mayank")
+for i in range(len(x)):
+    print(x[i])
